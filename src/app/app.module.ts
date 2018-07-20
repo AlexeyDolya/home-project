26e3903e25b0490c4d6d1caec2205959d3f18f5f
AlexeyDolya <alexeyjezzadolya@gmail.com>
2018-07-21 00:22:43 +0300

    header frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
